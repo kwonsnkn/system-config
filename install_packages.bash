de92010cff420972bd5b3905ffb268a07cc7a167
skwon <skwon@sk-vb-ubuntu-20.04-a>
2021-06-30 22:16:47 -0500

    Ubuntu system package installation

---
+++ install_packages.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+sudo apt install -y \
+build-essential \
+clang \
+cmake \
+curl \
+deepin-screenshot \
+dos2unix \
+g++ \
+gcc \
+git \
+imagemagick \
+make \
+net-tools \
+perl \
+python3-pip \
+python2 \
+python-cryptography \
+python3 \
+python3-cryptography \
+rsync \
+tmux 
+
